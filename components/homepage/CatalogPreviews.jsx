0eac4ba8bc7114e68de777a0e02b726bbbdc6c3a
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-06-29 21:15:53 +0300

    915

--- components/homepage/CatalogPreviews.jsx
+++ components/homepage/CatalogPreviews.jsx
@@ -54,6 +54,30 @@ export default function CatalogPreviews() {
           <ProductTile></ProductTile>
         </div>
       </section>
+
+      <section className="catalog-preview container flex-layout">
+        <header>
+          <h1>
+            <span>Wedding</span> Suits
+          </h1>
+
+          <p>
+            Accomplish new achievements with a bold new style from the brilliant
+            collection of mens designer suits. <br></br>
+            For the finest custom tailored experience, be sure to explore our
+            selection of premium fabrics from one of the worlds oldest fabric
+            mills, Vitale Barberis Canonico.
+          </p>
+        </header>
+
+        <div className="product-tiles">
+          <ProductTile></ProductTile>
+
+          <ProductTile></ProductTile>
+
+          <ProductTile></ProductTile>
+        </div>
+      </section>
     </>
   );
 }
