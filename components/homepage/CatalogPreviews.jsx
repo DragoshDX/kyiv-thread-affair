e930c1e9814903f79ba94956e6a952f5398311e2
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-06-29 19:29:51 +0300

    729

---
+++ components/homepage/CatalogPreviews.jsx
@@ -0,0 +1,21 @@
+export default function CatalogPreviews() {
+  return (
+    <>
+      <section className="catalog-preview">
+        <header>
+          <h1>
+            <span>Glamour</span> Suits
+          </h1>
+
+          <p>
+            Accomplish new achievements with a bold new style from the brilliant
+            collection of mens designer suits. <br></br>
+            For the finest custom tailored experience, be sure to explore our
+            selection of premium fabrics from one of the worlds oldest fabric
+            mills, Vitale Barberis Canonico.
+          </p>
+        </header>
+      </section>
+    </>
+  );
+}
